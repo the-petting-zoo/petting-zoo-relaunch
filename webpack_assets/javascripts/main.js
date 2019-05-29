ba6ba5d400b89ff5582f6671ad80fda242fb06c0
Jay Perry <j@jayperryworks.com>
2019-05-29 16:44:15 -0400

    [#109] set up main nav layout and dropdowns added vue to run it

---
+++ webpack_assets/javascripts/main.js
@@ -0,0 +1,22 @@
+// JS to keep:
+// - tabs
+// - contactForm
+// - filters?
+// - mobile nav
+
+// import flyout from './modules/flyout.js'
+
+// // flyout buttons
+// document.addEventListener('DOMContentLoaded', () => {
+//   // set up flyout toggle buttons
+//   flyout('data-js-toggle')
+// })
+
+import Vue from 'vue'
+
+new Vue({
+  el: '#main-nav-toggle',
+  data: {
+    active: false
+  }
+})
