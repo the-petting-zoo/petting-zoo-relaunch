fbb34b6f3db5511f641df72fff1ae4b64b076d71
Jay Perry <j@jayperryworks.com>
2019-06-17 17:34:20 -0400

    [#] new mail form working! form submits to simpleform

--- webpack_assets/javascripts/components/ContactForm.js
+++ webpack_assets/javascripts/components/ContactForm.js
@@ -3,66 +3,119 @@
 //  name="FNAME"
 
 import Vue from 'vue'
+import axios from 'axios'
 
 export default Vue.component('contact-form', {
   props: {
     url: String
   },
-  data() {
+  data () {
     return {
-      simpleFormToken: '9e785bffbf9337d08052b2b07bb8ef67',
-      simpleFormUrl: `http://getsimpleform.com/messages/ajax?form_api_token=9e785bffbf9337d08052b2b07bb8ef67`,
-      sent: false
+      simpleFormUrl: 'https://getsimpleform.com/messages?form_api_token=',
+      token: '9e785bffbf9337d08052b2b07bb8ef67',
+      testToken: 'd785a1918d67317a7cd4f65c805f1c61',
+      formContent: {},
+      sent: false,
+      subscribed: false
+    }
+  },
+  methods: {
+    submitForm (event) {
+      event.preventDefault()
+      axios.post(
+        `${this.simpleFormUrl}${this.token}`, 
+        this.formContent
+      ).then(response => {
+        console.log(`${response.status}: ${response.statusText}`)
+      })
+      this.sent = true
     }
   },
   template: `
     <div>
       <form v-if="!sent" action="#">
+
+        <!-- contact form -->
         <div class="control_group">
           <label for="name">Your Name</label>
-          <input type="text" class="form-textbox" data-type="input-textbox" id="name" size="20" value="" required/>
+          <input 
+            v-model="formContent.name"
+            type="text"
+            class="form-textbox"
+            id="contact-name"
+            size="20"
+            value=""
+            required
+          />
         </div>
         <div class="control_group">
           <label for="email">Your Email</label>
-          <input type="email" class="form-textbox" data-type="input-textbox" id="email" size="20" value="" required/>
+          <input
+            v-model="formContent.email"
+            type="email"
+            class="form-textbox"
+            id="contact-email"
+            size="20"
+            value=""
+            required
+          />
         </div>
         <div class="control_group">
-        <select class="form-dropdown" id="subject">
+        <select
+          v-model="formContent.subject"
+          class="form-dropdown"
+          id="contact-subject"
+        >
           <slot name="subject-options"></slot>
         </select>
         </div>
         <div class="control_group">
           <label for="message">Your Message</label>
-          <textarea id="message" class="form-textarea" name="message" cols="40" rows="6" required></textarea>
+          <textarea
+            v-model="formContent.message"
+            id="contact-message"
+            class="form-textarea"
+            cols="40"
+            rows="6"
+            required
+          ></textarea>
         </div>
-        <input type="hidden" class="form-hidden" :value="url" id="url" name="url" />
+        <input type="hidden" class="form-hidden" :value="url" id="contact-url" name="url" />
         <div class="control_group mailing-list">
-          <input type="checkbox" id="mailing-list" value="opted-in" />
+          <input type="checkbox" id="contact-mailing-list" value="opted-in" />
           <label for="mailing-list">Add me to <strong>The Petting Zoo's</strong> mailing&nbsp;list.</label>
         </div>
-        <button id="contact-submit" type="submit" class="button primary small">Send</button>
+        <button
+          @click="submitForm"
+          id="contact-contact-submit"
+          type="submit"
+          class="button primary small">
+            Send
+          </button>
       </form>
-
-      <form id="mc-form" style="display:none;">
-        <input id="mc-email" type="email" placeholder="email">
+      
+      <!-- Mailchimp/newsletter form -->
+      <form id="contact-mc-form" style="display:none;">
+        <input id="contact-mc-email" type="email" placeholder="email">
         <label for="mc-email"></label>
-        <input type="text" value="" class="" id="mc-FNAME">
+        <input type="text" value="" class="" id="contact-mc-FNAME">
         <label for="mc-FNAME"></label>
         <button type="submit">Submit</button>
       </form>
-
+      
+      <!-- Contact info sidebar -->
       <aside v-if="!sent" class="meta contact">
         <slot></slot>
       </aside>
-
+      
       <div v-if="sent" class="sent">
+        <!-- Success message -->
         <h3>Thanks for contacting us!</h3>
         <p class="center">We'll be in touch with you soon.</p>
+        <p v-if="subscribed" class="t-align-center">
+          Thank you for subscribing to our mailing list. We have sent you a confirmation email.
+        </p>
       </div>
-
-      <p v-if="sent" class="t-align-center">
-        Thank you for subscribing to our mailing list. We have sent you a confirmation email.
-      </p>
     </div>
   `
 })
