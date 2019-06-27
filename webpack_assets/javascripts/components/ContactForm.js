538797f7288caab4144bd45e04137e424586acf4
Scott Schaffter <scottschaffter@gmail.com>
2019-06-26 22:33:44 -0600

    Add validation and clean up console logs

--- webpack_assets/javascripts/components/ContactForm.js
+++ webpack_assets/javascripts/components/ContactForm.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import jsonp from 'jsonp'
 import queryString from 'query-string'
 
+// See README_ContactForm for details on simpleform configuration
+
 export default Vue.component('contact-form', {
   props: {
     url: String
@@ -9,53 +11,85 @@ export default Vue.component('contact-form', {
   data () {
     return {
       simpleForm: {
-        url: 'https://getsimpleform.com/messages/ajax?form_api_token=',
+        url: 'https://getsimpleform.com/messages/ajax?',
         token: '9e785bffbf9337d08052b2b07bb8ef67',
         testToken: 'd785a1918d67317a7cd4f65c805f1c61',
-        scottToken: '1697c4297b68bce50f046e36880ae4f8',
+        scottToken: '1697c4297b68bce50f046e36880ae4f8'
       },
       mailChimpUrl: 'https://gpoba.us8.list-manage.com/subscribe/post?u=0eb271cf853e657ebe61f0e9f&amp;id=a142a0b83f',
       formContent: {},
       sent: false,
       error: false,
-      subscribed: false
+      subscribed: false,
+      mcMessage:'',
+      errors: []
     }
   },
   methods: {
-    submitForm (event) {
+    checkForm: function (event) {
+      this.errors = []
       event.preventDefault()
+
+      if (!this.formContent.name) {
+        this.errors.push("Name required.")
+      }
+
+      if (!this.formContent.subject) {
+        this.errors.push("Email subject required.")
+      }
+
+      if (!this.formContent.message) {
+        this.errors.push("Email body required.")
+      }
+
+      if (!this.formContent.email) {
+        this.errors.push('Email required.')
+      } else if (!this.validEmail(this.formContent.email)) {
+        this.errors.push('Valid email required.')
+      }
+
+      if (!this.errors.length) {
+        this.submitForm()
+      }
+      
+    },
+    validEmail: function (email) {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    },
+    submitForm () {
+      // Add API token for simpleform (note form_api_token must remain the name of thie variable)
+      // convert to URL string
+      this.formContent.form_api_token = this.simpleForm.scottToken
       const urlData = queryString.stringify(this.formContent)
-      console.log(urlData)
 
       jsonp(
-        `${this.simpleForm.url}${this.simpleForm.scottToken}${urlData}`,
+        `${this.simpleForm.url}${urlData}`,
         null,
         (err, data) => {
           if (err) {
-            console.log("Error!")
-            console.error(err.message)
-            this.error = true
-            return false
+            this.errors.push(err.message)
+            this.sent = false
           } else {
-            console.log("Success Email")
             this.sent = true
+            // If checkbox for subscribe, then send to mailchimp
             if (this.subscribed) {
               this.subscribe()
             }
-            console.log(data)
           }
         }
       )
     },
     subscribe () {
-      const url = this.mailChimpUrl.replace("/post?", "/post-json?")
+      // @JAY 
+      // This will need to be updated with whatever fields PZP's MC has.
       const params = {
         EMAIL: this.formContent.email,
         FNAME: this.formContent.name.split(' ')[0],
         LNAME: this.formContent.name.split(' ')[1]
       }
-      const urlData = queryString.stringify(params)
-      console.log(urlData)
+      const url = this.mailChimpUrl.replace("/post?", "/post-json?")      
+      const urlData = queryString.stringify(this.params)
 
       jsonp(
         `${url}&${urlData}`,
@@ -63,14 +97,12 @@ export default Vue.component('contact-form', {
           param: "c"
         },
          (err, data) => {
-          if (err) {
-            console.log("Error!")
-            console.error(err.message)
-            this.error = true
+          if (err || data.result === "error") {
+            this.mcMessage = data.msg
+            this.errors.push(this.mcMessage)
             this.sent = false
           } else {
-            console.log("Success Subscribe")
-            console.log(data)
+            this.mcMessage = data.msg
           }
         }
       )
@@ -78,6 +110,15 @@ export default Vue.component('contact-form', {
   },
   template: `
     <div>
+    ${*/ @JAY YOU SHOULD PROBABLY THEME THIS ERROR FIELDS!*/}
+      <!-- Error message -->
+      <p v-if="errors.length">
+        <b>Please correct the following error(s):</b>
+        <ul>
+          <li v-for="error in errors">{{ error }}</li>
+        </ul>
+      </p>
+
       <form v-if="!sent" action="#">
 
         <!-- contact form -->
@@ -109,6 +150,7 @@ export default Vue.component('contact-form', {
         <select
           v-model="formContent.subject"
           class="form-dropdown"
+          name="subject"
           id="contact-subject"
         >
           <slot name="subject-options"></slot>
@@ -121,6 +163,7 @@ export default Vue.component('contact-form', {
             id="contact-message"
             class="form-textarea"
             cols="40"
+            name="message"
             rows="6"
             required
           ></textarea>
@@ -136,7 +179,7 @@ export default Vue.component('contact-form', {
           <label for="mailing-list">Add me to <strong>The Petting Zoo's</strong> mailing&nbsp;list.</label>
         </div>
         <button
-          @click="submitForm"
+          @click="checkForm"
           id="contact-contact-submit"
           type="submit"
           class="button primary small">
@@ -149,16 +192,7 @@ export default Vue.component('contact-form', {
         <h3>Thanks for contacting us!</h3>
         <p class="center">We'll be in touch with you soon.</p>
         <p v-if="subscribed && sent" class="t-align-center">
-          Thank you for subscribing to our mailing list. We have sent you a confirmation email.
-        </p>
-      </div>
-
-      <div v-if="error" class="error">
-        <!-- Success message -->
-        <h3>Thanks for contacting us!</h3>
-        <p class="center">We'll be in touch with you soon.</p>
-        <p v-if="subscribed && sent" class="t-align-center">
-          Thank you for subscribing to our mailing list. We have sent you a confirmation email.
+          {{mcMessage}} We have sent you a confirmation email.
         </p>
       </div>
       
---
+++ webpack_assets/javascripts/components/README_ContactForm.md
@@ -0,0 +1,22 @@
+Update The Simple form with the following data
+
+You will need to setup your Simple Form to support the fields included below.
+
+You can set this up by going to:
+https://getsimpleform.com/forms/[YOUR ACCESS API KEY]/edit
+
+=== Reply to field ===
+email
+
+=== Subject Template ===
+
+{{subject}}
+
+=== Body Template ===
+Dear Petting Zoo, 
+<br />
+{{message}}
+<br />
+Regards, 
+<br />
+{{name}}
